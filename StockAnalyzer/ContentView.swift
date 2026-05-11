ff6ca03cf14d08d9ef37bc64c096346ed26f0e67
leeshengcian <sclee.cs12@nycu.edu.tw>
2026-05-11 15:44:11 +0800

    Initial Commit

---
+++ StockAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockAnalyzer
+//
+//  Created by 李昇謙 on 2026/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockAnalyzer/StockAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAnalyzerApp.swift
+//  StockAnalyzer
+//
+//  Created by 李昇謙 on 2026/5/11.
+//
+
+import SwiftUI
+
+@main
+struct StockAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
